0640a4f0ac3e628c1820c4dae2c2f31d29c6fb47
Baishenliang <liangbs95@gmail.com>
2025-01-05 17:36:31 -0500

    start to gamma for D117

--- sbatch_preproc.sh
+++ sbatch_preproc.sh
@@ -2,7 +2,7 @@
 #SBATCH -e data/DCCbatchout/slurm_%a.err
 #SBATCH -a 115-120%20
 #SBATCH --mem=800G
-#SBATCH --cpus-per-task=80
+#SBATCH --cpus-per-task=30
 #SBATCH --partition=common,scavenger
 #SBATCH --output=data/DCCbatchout/test%a.out
 #conda init bash
