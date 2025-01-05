134d6ac31c9149a29ac310cef21b770696c66978
Baishenliang <liangbs95@gmail.com>
2025-01-05 16:48:39 -0500

    start to multitaper for D117

--- sbatch_preproc.sh
+++ sbatch_preproc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -e data/DCCbatchout/slurm_%a.err
 #SBATCH -a 115-120%20
-#SBATCH --mem=300G
-#SBATCH --cpus-per-task=20
+#SBATCH --mem=800G
+#SBATCH --cpus-per-task=80
 #SBATCH --partition=common,scavenger
 #SBATCH --output=data/DCCbatchout/test%a.out
 #conda init bash
