2249e274c592aba6dfe3b129ae612e68529b077b
Baishenliang <liangbs95@gmail.com>
2025-01-05 13:56:36 -0500

    Do D117 line noise, outlier, and wavelet

--- sbatch_preproc.sh
+++ sbatch_preproc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -e data/DCCbatchout/slurm_%a.err
 #SBATCH -a 30-50%20
-#SBATCH --mem=300G
-#SBATCH --cpus-per-task=20
+#SBATCH --mem=400G
+#SBATCH --cpus-per-task=40
 #SBATCH --partition=common,scavenger
 #SBATCH --output=data/DCCbatchout/test%a.out
 #conda init bash
