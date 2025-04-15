4308e9018c54dad68e1426fa8650dca79ad4dba1
Baishenliang <liangbs95@gmail.com>
2025-04-15 15:31:58 -0400

    Redo D24 preprocessing

--- sbatch_preproc.sh
+++ sbatch_preproc.sh
@@ -2,7 +2,7 @@
 #SBATCH -e data/DCCbatchout/slurm_%a.err
 #SBATCH -a 20-25%20
 #SBATCH --mem=600G
-#SBATCH --cpus-per-task=100
+#SBATCH --cpus-per-task=10
 #SBATCH --partition=common,scavenger
 #SBATCH --output=data/DCCbatchout/test%a.out
 #conda init bash
