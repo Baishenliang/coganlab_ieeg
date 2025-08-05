52f3b48f9bf06877787e3b12fbcf31c508edad11
Baishenliang <liangbs95@gmail.com>
2025-08-05 16:19:23 -0400

    PCA-LDA： decode on different groups of electrodes. Time-bined, and do permutation test. Batch mode

--- projects/PCA_LDA/sbatch_pca_lda.sh
+++ projects/PCA_LDA/sbatch_pca_lda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -e ~/workspace/PCA_LDA_results/slurm_%a.err
 #SBATCH -a 1-30%20
-#SBATCH --mem=30G
+#SBATCH --mem=10G
 #SBATCH --cpus-per-task=20
 #SBATCH --partition=common,scavenger
 #SBATCH --output=~/workspace/PCA_LDA_results/test%a.out
