3a06817b8c2680be7c652720c716f271f6c8bcc1
Baishenliang <liangbs95@gmail.com>
2025-08-12 13:15:35 -0400

    Run LME on clusters: sbatch mode

--- projects/lme/sbatch_lme.sh
+++ projects/lme/sbatch_lme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -e DCC_out/slurm_%a.err
 #SBATCH -a 1-20
-#SBATCH --mem=60G
+#SBATCH --mem=80G
 #SBATCH --cpus-per-task=20
 #SBATCH --partition=common,scavenger
 #SBATCH --output=DCC_out/test%a.out
