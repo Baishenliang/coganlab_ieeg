771b166c8367f85dba68afe2647e66b09819c7f9
Baishenliang <liangbs95@gmail.com>
2025-08-14 15:05:00 -0400

    pho lme ~ fea + (1 | subject) + (1|electrode), Auditory_delay

--- projects/lme/lme_encode.R
+++ projects/lme/lme_encode.R
@@ -111,7 +111,7 @@ model_func <- function(current_data){
 #%% Parameters
 set.seed(42)
 phase<-'full'
-elec_grp <- 'Auditory_all'
+elec_grp <- 'Auditory_delay'
 align_to_onsets <- c('pho0')
 features <- c('pho1', 'pho2', 'pho3', 'pho4', 'pho5')
 post_align_T_threshold <- c(-0.2, 1)
