bb13e367135bf9deac9d2f61a7b38bb610c7324e
Baishenliang <liangbs95@gmail.com>
2025-10-29 14:47:09 -0400

    GLM HG: increased permutation size.

--- projects/lme/lme_encode.R
+++ projects/lme/lme_encode.R
@@ -89,7 +89,7 @@ model_func <- function(current_data,feature){
 
   # Permutation
   cat('Start perm \n')
-  n_perm <- 2e2
+  n_perm <- 1e3
   
   for (i_perm in 1:n_perm) {
     set.seed(10000 + i_perm)
