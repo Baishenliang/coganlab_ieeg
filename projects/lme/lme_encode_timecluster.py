6448ab32b546179e6cedc09c3a6c6745108f9fdb
Baishenliang <liangbs95@gmail.com>
2025-11-07 22:26:25 -0500

    Feature GLM: updated plotting.

--- projects/lme/lme_encode_timecluster.py
+++ projects/lme/lme_encode_timecluster.py
@@ -6,11 +6,10 @@ import numpy as np
 import pandas as pd
 from ieeg.calc.stats import time_cluster
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from partd.utils import suffix
 from requests.packages import target
 from statsmodels.stats.multitest import multipletests
-
-
 script_dir = os.path.dirname('D:\\bsliang_Coganlabcode\\coganlab_ieeg\\projects\\lme\\prepare_raw.py')
 current_dir = os.getcwd()
 if current_dir != script_dir:
@@ -39,7 +38,7 @@ def expand_sequence(prefix: str, suffix: str, max_number: int) -> tuple:
     expanded_list = [f'{prefix}{i}{suffix}' for i in range(1, max_number + 1)]
     return tuple(expanded_list)
 
-def get_traces_clus(raw, alpha:float=0.05, alpha_clus:float=0.05,mode:str='time_cluster',target_fea:str=None):
+def get_traces_clus(raw, alpha:float=0.05, alpha_clus:float=0.05,mode:str='time_cluster',target_fea:str=None,input:str='R2'):
     # Load data
     # aud_delay_org = pd.read_csv('Aud_delay_org.csv')
     # aud_delay_perm = pd.read_csv('Aud_delay_perm.csv')
@@ -78,37 +77,44 @@ def get_traces_clus(raw, alpha:float=0.05, alpha_clus:float=0.05,mode:str='time_
     # null_r2_i, 2-d time series of original glm values: n_perm*time
     r2_i=r2s_i[0,:]
     r2_i = np.expand_dims(r2_i, axis=0)
-    null_r2_i=r2s_i[1:,:]
+    if input=='R2':
+        null_r2_i=r2s_i[1:,:]
 
-    # Get original mask
-    org_p_i = glm.aaron_perm_gt_1d(r2s_i, axis=0)[0] # 1-d time series
-    mask_i_org = (org_p_i > (1 - alpha)).astype(int) # 1-d time series (binary)
+        # Get original mask
+        org_p_i = glm.aaron_perm_gt_1d(r2s_i, axis=0)[0] # 1-d time series
+        mask_i_org = (org_p_i > (1 - alpha)).astype(int) # 1-d time series (binary)
 
-    # Get null mask
-    null_p_i = glm.aaron_perm_gt_1d(null_r2_i, axis=0) # 2-d time series: n_perm*time
-    mask_null_i=(null_p_i>(1-alpha)).astype(int) # # 2-d time series: n_perm*time (binary)
+        # Get null mask
+        null_p_i = glm.aaron_perm_gt_1d(null_r2_i, axis=0) # 2-d time series: n_perm*time
+        mask_null_i=(null_p_i>(1-alpha)).astype(int) # # 2-d time series: n_perm*time (binary)
 
-    if mode == 'time_cluster':
-        # Time perm cluster
-        stat_out = time_cluster(mask_i_org, mask_null_i,1 - alpha_clus)
-    elif mode == 'fdr':
-        #fdr
-        stat_out, p_fdr, _, _ = multipletests(1-org_p_i, alpha=alpha_clus, method='fdr_bh')
+        if mode == 'time_cluster':
+            # Time perm cluster
+            stat_out = time_cluster(mask_i_org, mask_null_i,1 - alpha_clus)
+        elif mode == 'fdr':
+            #fdr
+            stat_out, p_fdr, _, _ = multipletests(1-org_p_i, alpha=alpha_clus, method='fdr_bh')
 
-    return time_point,r2_i[0],stat_out
+        return time_point,r2_i[0],stat_out
+    elif input=='p':
+        stat_out, p_fdr, _, _ = multipletests(r2_i[0], alpha=alpha_clus, method='fdr_bh')
+
+        return time_point,r2_i[0],stat_out
+
 
 #%% Plotting
 colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
           '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 is_normalize=False
+is_bsl_correct=True
 mode='time_cluster'
 #for elec_grp in ['Hickok_Spt','Hickok_lPMC','Hickok_lIFG']:
 # for elec_grp in ['Auditory_delay','Sensorymotor_delay','Motor_delay','Delay_only']:
 baseline=dict()
 baseline_std=dict()
 for alignment,xlim_align in zip(
-        ('aud','go','resp'),
-        ([-0.2, 1.75],[-0.2, 1],[-0.2, 1])):
+        ('aud',),#'go','resp'),
+        ([-0.2, 1.75],)):#[-0.2, 1],[-0.2, 1])):
     for elec_grp,elec_col in zip(('Auditory','Sensorymotor','Motor'),
                                  (Auditory_col,Sensorimotor_col,Motor_col))  :
     # for elec_grp in ['Auditory_delay','Sensorymotor_delay']:
@@ -126,7 +132,7 @@ for alignment,xlim_align in zip(
         #                                          ([0.13, 0.001],[0.13, 0.001],[0.13, 0.001],[0.13, 0.001],[0.01, 0.001])):
         for fea, fea_tag, para_sig_barbar in zip(('R2',),
                                                  ('R2',),
-                                                 ([0.012, 0.001],)):
+                                                 ([0.011, 0.001],)):
         # for fea, fea_tag, para_sig_barbar in zip(('aco','pho'),
         #                                          ('Acoustic','Phonemic'),
         #                                          ([2, 0.1],[0.1, 0.03])):
@@ -140,7 +146,7 @@ for alignment,xlim_align in zip(
         #                                          ('Vowel', 'Consonant'),
         #                                          ([0.8, 0.03], [0.8, 0.03])):
 
-            fig, ax = plt.subplots(figsize=(6*(xlim_align[1]-xlim_align[0]), 5))
+            fig, ax = plt.subplots(figsize=(5.6*(xlim_align[1]-xlim_align[0]), 5))
 
             ax.axvline(x=0, color='grey', linestyle='--', alpha=0.7,linewidth=3)
             if alignment == 'aud':
@@ -151,7 +157,13 @@ for alignment,xlim_align in zip(
             raw = pd.read_csv(filename)
 
             j = 0
-            for vWM,vwm_linestyle,vwm_text in zip(('vWM','novWM'),('-','--'),(' with delay',' without delay')):
+            for vWM,vwm_linestyle,input,pthres,vwm_text,sig_bar_col in zip(
+                    ('vWM','novWM','diff'),
+                    ('-','--','-.'),
+                    ('R2','R2','R2'),
+                    ([5e-2,9e-1],[5e-2,9e-1],[5e-2,9e-1]),
+                    ('vWM','nvWM','diff'),
+                    (elec_col,elec_col,[0.5,0.5,0.5])):
                 if fea == 'aco':
                     target_fea = list(expand_sequence('aco',vWM,9))
                 elif fea == 'pho':
@@ -160,9 +172,9 @@ for alignment,xlim_align in zip(
                     target_fea = list(expand_sequence('wordnessWord.pho',vWM,11))
                 elif fea == 'wordnessWord':
                     target_fea = fea+vWM
-                else:
+                elif fea == 'R2':
                     target_fea = fea+f'_{vWM}'
-                time_point, time_series, mask_time_clus = get_traces_clus(raw, 2.5e-2, 1e-1,mode=mode,target_fea=target_fea)
+                time_point, time_series, mask_time_clus = get_traces_clus(raw, pthres[0], pthres[1],mode=mode,target_fea=target_fea,input=input)
 
                 time_series=gaussian_filter1d(time_series, sigma=2, mode='nearest')
                 # win_len=10
@@ -175,7 +187,8 @@ for alignment,xlim_align in zip(
                     time_series = time_series/ (np.max(time_series[(time_point > xlim_align[0]) & (time_point <= xlim_align[1])]) - np.min((time_point > xlim_align[0]) & (time_point <= xlim_align[1])))
                     para_sig_bar = [1,1e-1]
                 else:
-                    time_series = (time_series - baseline[elec_grp])  # / (np.max(time_series) - np.min(time_series))
+                    if is_bsl_correct:
+                        time_series = (time_series - baseline[elec_grp])
                     para_sig_bar = para_sig_barbar
 
                 ax.plot(time_point, time_series, label=f"{elec_grp}{vwm_text}", color=elec_col, linewidth=5,linestyle=vwm_linestyle)
@@ -193,11 +206,19 @@ for alignment,xlim_align in zip(
                         end_time = time_point[end_index] + time_step / 2
 
                         label = f'clust{k} of pho'
-
-                        ax.plot([start_time, end_time], [para_sig_bar[0]-para_sig_bar[1]*(j-1),para_sig_bar[0]-para_sig_bar[1]*(j-1)],
-                                color=elec_col,alpha=0.4,
-                                linewidth=10,  # Make the line thick like a bar
-                                solid_capstyle='butt')  # Makes the line ends flat
+                        if vWM == 'vWM' or  vWM == 'novWM' :
+                            ax.plot([start_time, end_time], [para_sig_bar[0]-para_sig_bar[1]*(j-1),para_sig_bar[0]-para_sig_bar[1]*(j-1)],
+                                    color=sig_bar_col,alpha=0.4,
+                                    linewidth=10,  # Make the line thick like a bar
+                                    solid_capstyle='butt')  # Makes the line ends flat
+                        elif vWM == 'diff':
+                            ax.axvspan(
+                                xmin=start_time,
+                                xmax=end_time,
+                                facecolor=sig_bar_col,
+                                alpha=0.2,
+                                edgecolor='none'
+                            )
                 j=j+1
 
             # ax.set_title(f"{fea_tag}", fontsize=24)
@@ -207,12 +228,20 @@ for alignment,xlim_align in zip(
             # else:
             #     ax.set_ylabel("feature $β$")#, fontsize=20)
             ax.tick_params(axis='both', which='major')#, labelsize=16)
+            ax.ticklabel_format(
+                axis='y',
+                style='sci',
+                scilimits=(-3, -3),  # 将指数固定为 10^-3
+                useMathText=True  # 使用 LaTeX 格式显示指数，如 10⁻³
+            )
+            ax.xaxis.set_major_locator(ticker.MultipleLocator(0.25))
+            ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
             ax.legend(fontsize=18)
             ax.set_xlim(xlim_align)#time_point.max())
             ax.legend().set_visible(False)
             ax.spines['top'].set_visible(False)
             ax.spines['right'].set_visible(False)
-            ax.set_ylim(-0.005,para_sig_bar[0]+2*para_sig_bar[1])
+            ax.set_ylim(-0.002,para_sig_bar[0]+2*para_sig_bar[1])
             plt.tight_layout()
-            plt.savefig(os.path.join('figs', f'{elec_grp}_{fea_tag}_{alignment}.tif'), dpi=300)
+            plt.savefig(os.path.join('figs','multencode', f'{elec_grp}_{fea_tag}_{alignment}.tif'), dpi=300)
             plt.close()
