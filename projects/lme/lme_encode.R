e18363406988fbd66fa0f98f7559621f2ecdff06
Baishenliang <liangbs95@gmail.com>
2025-12-19 19:13:28 -0500

    Do encoding for sub groups of SM electrodes with optimized lambdas

--- projects/lme/lme_encode.R
+++ projects/lme/lme_encode.R
@@ -184,7 +184,7 @@ model_func <- function(current_data){
   
   # Permutation
   cat('Start perm \n')
-  n_perm <- 0#3e2#1e3
+  n_perm <- 3e2#1e3
   
   if (n_perm>0){
     for (i_perm in 1:n_perm) {
@@ -272,16 +272,24 @@ ridge_lambda_speech <- data.frame( # lambda adjusted according to electrode size
           1, # Motor vWM
           10, # Delay only vWM
           0.001,# Wgw_p55b
-          0.001),# Wgw_a55b
+          0.001,# Wgw_a55b
+          1e-5, #SM_vWM_Auditory_early
+          0.1,# SM_vWM_Auditory_late
+          0.001,# SM_vWM_Delay
+          0.1),  # SM_vWM_Motor
   
   novWM = c(100, # Auditory novWM
             40, # Sensorymotor novWM
             450,# Motor novWM
             20,  # Delay only novWM (Sensorymotor novWM)
             10,  # Wgw_p55b
-            10)# Wgw_a55b
+            10,# Wgw_a55b
+            1e-5, #SM_vWM_Auditory_early
+            0.1,# SM_vWM_Auditory_late
+            0.001,# SM_vWM_Delay
+            0.1)  # SM_vWM_Motor
 )
-rownames(ridge_lambda_speech) <- c("Auditory", "Sensorymotor", "Motor","Delay_only",'Wgw_p55b','Wgw_a55b')
+rownames(ridge_lambda_speech) <- c("Auditory", "Sensorymotor", "Motor","Delay_only",'Wgw_p55b','Wgw_a55b','SM_vWM_Auditory_early','SM_vWM_Auditory_late','SM_vWM_Delay','SM_vWM_Motor')
 
 
 ridge_lambda_semantics <- data.frame( # lambda adjusted according to electrode size
@@ -337,8 +345,8 @@ sem_fea_T$stim <- rownames(sem_fea_T)
 sem_fea_T <- sem_fea_T[, c("stim", setdiff(names(sem_fea_T), "stim"))]
 
 #%% Start looping
-#for (ridge_lambda in list(ridge_lambda_speech)){#list(ridge_lambda1,ridge_lambda2)){
-for (lambda_test in c(0.00001,0.0001,0.001,0.01,0.1,1,10,100,1000,10000)){
+for (ridge_lambda in list(ridge_lambda_speech)){#list(ridge_lambda1,ridge_lambda2)){
+#for (lambda_test in c(0.00001,0.0001,0.001,0.01,0.1,1,10,100,1000,10000)){
 #for (lambda_test in c(20,40,60,80,200,500,1000,10000)){
 #for (lambda_test in c(0.2,0.4,0.6,0.8,2,4,6,8)){
   for (delay_nodelay in delay_nodelays){
@@ -425,13 +433,13 @@ for (lambda_test in c(0.00001,0.0001,0.001,0.01,0.1,1,10,100,1000,10000)){
         #%% Run computations
         
         #%% append ridge lambdas
-        # word_data$ridge_lambda_vWM<-ridge_lambda[elec_grp,'vWM']
-        # word_data$ridge_lambda_novWM<-ridge_lambda[elec_grp,'novWM']
+        word_data$ridge_lambda_vWM<-ridge_lambda[elec_grp,'vWM']
+        word_data$ridge_lambda_novWM<-ridge_lambda[elec_grp,'novWM']
         # current_pair <- all_pairs[lambda_pair, ]
         # word_data$ridge_lambda_vWM <- current_pair$First_Number
         # word_data$ridge_lambda_novWM <- current_pair$Second_Number
-        word_data$ridge_lambda_vWM<-lambda_test
-        word_data$ridge_lambda_novWM<-lambda_test
+        # word_data$ridge_lambda_vWM<-lambda_test
+        # word_data$ridge_lambda_novWM<-lambda_test
         cat("Re-formatting long data \n")
         data_by_time <- split(word_data, word_data$time)
         rm(word_data)
@@ -451,8 +459,8 @@ for (lambda_test in c(0.00001,0.0001,0.001,0.01,0.1,1,10,100,1000,10000)){
         
         print(perm_compare_df)
         
-        #write.csv(perm_compare_df,paste(home_dir,"results/",delay_nodelay,"_",elec_grp,"_",alignment,"_",lex,"_rawpow_vWMλ_",ridge_lambda[elec_grp,'vWM'],"_novWMλ_",ridge_lambda[elec_grp,'novWM'],".csv",sep = ''),row.names = FALSE)
-        write.csv(perm_compare_df,paste(home_dir,"results/",delay_nodelay,"_",elec_grp,"_",alignment,"_",lex,"_rawpow_testλ_",lambda_test,".csv",sep = ''),row.names = FALSE)
+        write.csv(perm_compare_df,paste(home_dir,"results/",delay_nodelay,"_",elec_grp,"_",alignment,"_",lex,"_rawpow_vWMλ_",ridge_lambda[elec_grp,'vWM'],"_novWMλ_",ridge_lambda[elec_grp,'novWM'],".csv",sep = ''),row.names = FALSE)
+        #write.csv(perm_compare_df,paste(home_dir,"results/",delay_nodelay,"_",elec_grp,"_",alignment,"_",lex,"_rawpow_testλ_",lambda_test,".csv",sep = ''),row.names = FALSE)
   
         }
       }
