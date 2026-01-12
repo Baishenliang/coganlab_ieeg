9f43a789881b325684f8f6afe335d9f7e4fdd31f
Baishenliang <liangbs95@gmail.com>
2026-01-12 00:15:29 -0500

    glm semantic only higher permutation

--- projects/lme/lme_encode.R
+++ projects/lme/lme_encode.R
@@ -151,8 +151,8 @@ model_func <- function(current_data){
   term_sem <- paste0('sem', 1:75)
   
   #all_terms <- c(term_aco, term_pho, term_sem, "wordness")
-  all_terms <- c(term_aco, term_pho, "wordness")
-  #all_terms <- c(term_sem)
+  #all_terms <- c(term_aco, term_pho, "wordness")
+  all_terms <- c(term_sem)
   
   fml <- as.formula(paste0("value ~ 1 + ", paste(all_terms, collapse = " + ")))
   
@@ -419,7 +419,7 @@ for (rep_yn in c('_forSilence')){#,'','_yn')){
         print(perm_compare_df)
         
         #write.csv(perm_compare_df,paste(home_dir,"results/",delay_nodelay,"_",elec_grp,"_",alignment,"_",lex,"_vWMλ_",ridge_lambda[elec_grp,'vWM'],".csv",sep = ''),row.names = FALSE)
-        write.csv(perm_compare_df,paste(home_dir,"results/",delay_nodelay,"_",elec_grp,"_",alignment,"_",lex,rep_yn,"_testλ_",lambda_test,".csv",sep = ''),row.names = FALSE)
+        write.csv(perm_compare_df,paste(home_dir,"results/",delay_nodelay,"_",elec_grp,"_",alignment,"_",lex,rep_yn,"_onlysem_testλ_",lambda_test,".csv",sep = ''),row.names = FALSE)
         
       }
     }
