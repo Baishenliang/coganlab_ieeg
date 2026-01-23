6adf0d5329608ceb564b2e250f2ce88cfcbdceb6
Baishenliang <liangbs95@gmail.com>
2026-01-23 10:52:26 -0500

    redo preproc for nodelay D134

--- sbatch_preproc.sh
+++ sbatch_preproc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -e data/DCCbatchout/slurm_%a.err
-#SBATCH -a 100-140
-#SBATCH --mem=150G
+#SBATCH -a 130-140
+#SBATCH --mem=300G
 #SBATCH --cpus-per-task=10
 #SBATCH --partition=common,scavenger
 #SBATCH --output=data/DCCbatchout/test%a.out
