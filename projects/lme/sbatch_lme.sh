1bf2fc0073502c4b0b51286471315cbceaed8261
Baishenliang <liangbs95@gmail.com>
2026-04-20 23:59:07 -0400

    updated lme parameters

--- projects/lme/sbatch_lme.sh
+++ projects/lme/sbatch_lme.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -e DCC_out/%a.err
-#SBATCH -a 1-10
-#SBATCH --mem=160G
-#SBATCH --cpus-per-task=30
+#SBATCH -a 1,2,6
+#SBATCH --mem=320G
+#SBATCH --cpus-per-task=40
 #SBATCH --partition=common,scavenger
 #SBATCH --output=DCC_out/%a.out
 module load R
